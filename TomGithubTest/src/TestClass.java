d6f5559087b95185e02ad795a83504ff617ee675
tomEdwards <tomo12321@hotmail.com>
2016-09-13 17:56:05 +1200

    TRYNA be a dog

--- TomGithubTest/src/TestClass.java
+++ TomGithubTest/src/TestClass.java
@@ -1,9 +1,13 @@
 
 public class TestClass {
+	
+	public TestClass(){
+		System.out.println("What up my young but youthful nigs");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new TestClass();
 	}
 
 }
