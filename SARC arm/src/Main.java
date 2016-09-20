a048331ca8a8997a4b204892f2ae53ca7d43100f
tomEdwards <tomo12321@hotmail.com>
2016-09-20 14:05:03 +1200

    Updated

--- SARC arm/src/Main.java
+++ SARC arm/src/Main.java
@@ -1,177 +1,207 @@
 
-
 /* Code for Assignment ?? 
  * Name:
  * Usercode:
  * ID:
  */
 
-
 import ecs100.*;
 import java.util.*;
 import java.io.*;
 import java.awt.*;
 
-
-/** <description of class Main>
+/**
+ * <description of class Main>
  */
-public class Main{
+public class Main {
 
-    private Arm arm;
-    private Drawing drawing;
-    private ToolPath tool_path;
-    // state of the GUI
-    private int state; // 0 - nothing
-                       // 1 - inverse point kinematics - point
-                       // 2 - enter path. Each click adds point  
-                       // 3 - enter path pause. Click does not add the point to the path
-    
-    /**      */
-    public Main(){
-        UI.initialise();
-        UI.setWindowSize(1200, 800);
-        UI.addButton("xy to angles", this::inverse);
-        UI.addButton("Enter path XY", this::enter_path_xy);
-        UI.addButton("Save path XY", this::save_xy);
-        UI.addButton("Load path XY", this::load_xy);
-        UI.addButton("Save path Ang", this::save_ang);
-        UI.addButton("Load path Ang:Play", this::load_ang);
-        UI.addButton("SEND thing", this::doThing);
-                
-       // UI.addButton("Quit", UI::quit);
-        UI.setMouseMotionListener(this::doMouse);
-        UI.setKeyListener(this::doKeys);
+	private Arm arm;
+	private Drawing drawing;
+	private ToolPath tool_path;
+	// state of the GUI
+	private int state; // 0 - nothing
+	// 1 - inverse point kinematics - point
+	// 2 - enter path. Each click adds point
+	// 3 - enter path pause. Click does not add the point to the path
 
+	public ArrayList<String> toSendText = new ArrayList<String>();
 
-        //ServerSocket serverSocket = new ServerSocket(22); 
-        
-        this.arm = new Arm();
-        this.drawing = new Drawing();
-        this.run();
-        arm.draw();
-    }
-    
-    public void doThing(){
-    	
-    }
-    
-    public void doKeys(String action){
-        UI.printf("Key :%s \n", action);
-        if (action.equals("b")) {
-            // break - stop entering the lines
-            state = 3;
-            //
-          
-        }
-               
-    }
-    
-    
-    public void doMouse(String action, double x, double y) {
-         //UI.printf("Mouse Click:%s, state:%d  x:%3.1f  y:%3.1f\n",
-         //   action,state,x,y);
-        UI.clearGraphics();
-        String out_str=String.format("%3.1f %3.1f",x,y);
-        UI.drawString(out_str, x+10,y+10);
-         // 
-         if ((state == 1)&&(action.equals("clicked"))){
-          // draw as 
-          
-          arm.inverseKinematic(x,y);
-          arm.draw();
-          return;
-        }
-        
-         if ( ((state == 2)||(state == 3))&&action.equals("moved") ){
-          // draw arm and path
-          arm.inverseKinematic(x,y);
-          arm.draw();
-         
-          // draw segment from last entered point to current mouse position
-          if ((state == 2)&&(drawing.get_path_size()>0)){
-            PointXY lp = new PointXY();
-            lp = drawing.get_path_last_point();
-            //if (lp.get_pen()){
-               UI.setColor(Color.GRAY);
-               UI.drawLine(lp.get_x(),lp.get_y(),x,y);
-           // }
-          }
-           drawing.draw();
-        }
-        
-        // add point
-        if (   (state == 2) &&(action.equals("clicked"))){
-            // add point(pen down) and draw
-            UI.printf("Adding point x=%f y=%f\n",x,y);
-            drawing.add_point_to_path(x,y,true); // add point with pen down
-            
-            arm.inverseKinematic(x,y);
-            arm.draw();
-            drawing.draw();
-            drawing.print_path();
-        }
-        
-        
-        if (   (state == 3) &&(action.equals("clicked"))){
-            // add point and draw
-            //UI.printf("Adding point x=%f y=%f\n",x,y);
-            drawing.add_point_to_path(x,y,false); // add point wit pen up
-            
-            arm.inverseKinematic(x,y);
-            arm.draw();
-            drawing.draw();
-            drawing.print_path();
-            state = 2;
-        }
-        
-        
-    }
-   
-    
-    public void save_xy(){
-        state = 0;
-        String fname = UIFileChooser.save();
-        drawing.save_path(fname);
-    }
-    
-    public void enter_path_xy(){
-         state = 2;
-    }
-    
-    public void inverse(){
-         state = 1;
-         arm.draw();
-    }
-    
-    public void load_xy(){
-        state = 0;
-        String fname = UIFileChooser.open();
-        drawing.load_path(fname);
-        drawing.draw();
-        
-        arm.draw();
-    }
-    
-    // save angles into the file
-    public void save_ang(){
-        String fname = UIFileChooser.open();
-        tool_path.convert_drawing_to_angles(drawing,arm,fname);
-    }
-    
-    
-    public void load_ang(){
-        
-    }
-    
-    public void run() {
-        while(true) {
-            arm.draw();
-            UI.sleep(20);
-        }
-    }
+	/**      */
+	public Main() {
+		UI.initialise();
+		UI.setWindowSize(1200, 800);
+		UI.addButton("xy to angles", this::inverse);
+		UI.addButton("Enter path XY", this::enter_path_xy);
+		UI.addButton("Save path XY", this::save_xy);
+		UI.addButton("Load path XY", this::load_xy);
+		UI.addButton("Save path Ang", this::save_ang);
+		UI.addButton("Load path Ang:Play", this::load_ang);
+		UI.addButton("SEND file", this::doSendFile);
 
-    public static void main(String[] args){
-        Main obj = new Main();
-    }    
+		// UI.addButton("Quit", UI::quit);
+		UI.setMouseMotionListener(this::doMouse);
+		UI.setKeyListener(this::doKeys);
+
+		// ServerSocket serverSocket = new ServerSocket(22);
+		this.arm = new Arm();
+		this.drawing = new Drawing();
+		this.run();
+		arm.draw();
+	}
+
+	/**
+	 * Starts the toolPath class, which creates a set of steps that the motor should move to in order to produce
+	 * straight lines between points.
+	 */
+	public void doSendFile() {
+
+		tool_path = new ToolPath();
+		tool_path.convert_drawing_to_angles(drawing, arm, "does nothing");
+		tool_path.convert_angles_to_pwm(arm);
+		tool_path.save_pwm_file();
+		/**
+		 * try { tool_path = new ToolPath(); tool_path.convert_drawing_to_angles(drawing, arm, "does nothing");
+		 * tool_path.convert_angles_to_pwm(arm); tool_path.save_pwm_file();
+		 * 
+		 * Runtime.getRuntime().exec("scp PWMToSend pi@10.140.141.22:/home/pi/arm");// sends the text file "PWMToSend"
+		 * // to the pi. I think can only // be executed on lab computers // MIGHT need to add passwords } catch
+		 * (IOException e) { UI.println("Something went wrong " + e); }
+		 */
+	}
+
+	public void doKeys(String action) {
+		UI.printf("Key :%s \n", action);
+		if (action.equals("b")) {
+			// break - stop entering the lines
+			state = 3;
+			//
+
+		}
+
+	}
+
+	public void doMouse(String action, double x, double y) {
+		// UI.printf("Mouse Click:%s, state:%d x:%3.1f y:%3.1f\n",
+		// action,state,x,y);
+		UI.clearGraphics();
+		String out_str = String.format("%3.1f %3.1f", x, y);
+		UI.drawString(out_str, x + 10, y + 10);
+		//
+		if ((state == 1) && (action.equals("clicked"))) {
+			// draw as
+
+			arm.inverseKinematic(x, y);
+			arm.draw();
+			return;
+		}
+
+		if (((state == 2) || (state == 3)) && action.equals("moved")) {
+			// draw arm and path
+			arm.inverseKinematic(x, y);
+			// toSendText.add(String.valueOf(arm.get_theta1())+ ","+String.valueOf(arm.get_theta2())+",0");//0 is
+			// up/down pen
+			arm.draw();
+
+			// draw segment from last entered point to current mouse position
+			if ((state == 2) && (drawing.get_path_size() > 0)) {
+				PointXY lp = new PointXY();
+				lp = drawing.get_path_last_point();
+				// if (lp.get_pen()){
+				UI.setColor(Color.GRAY);
+				UI.drawLine(lp.get_x(), lp.get_y(), x, y);
+				// }
+			}
+			drawing.draw();
+		}
+
+		// add point
+		if ((state == 2) && (action.equals("clicked"))) {
+			// add point(pen down) and draw
+			UI.printf("Adding point x=%f y=%f\n", x, y);
+			drawing.add_point_to_path(x, y, true); // add point with pen down
+
+			arm.inverseKinematic(x, y);
+			toSendText.add(this.convertToPWM(arm.get_theta1(), true) + "," + this.convertToPWM(arm.get_theta2(), false)
+					+ ",2000");// 2000 is up/down pen
+			arm.draw();
+			drawing.draw();
+			drawing.print_path();
+		}
+
+		if ((state == 3) && (action.equals("clicked"))) {
+			// add point and draw
+			// UI.printf("Adding point x=%f y=%f\n",x,y);
+			drawing.add_point_to_path(x, y, false); // add point wit pen up
+
+			arm.inverseKinematic(x, y);
+			toSendText.add(this.convertToPWM(arm.get_theta1(), true) + "," + this.convertToPWM(arm.get_theta2(), false)
+					+ ",2000");// 2000 is up/down pen
+			arm.draw();
+			drawing.draw();
+			drawing.print_path();
+			state = 2;
+		}
+
+	}
+
+	public String convertToPWM(double angle, boolean motor1) {
+		// angle is between 0 - -3.14 radians
+		// angle should be converted into PWM String value 0 for 0 radian angle, each degree extra corresponds to about
+		// 10PWM, increase
+		angle = -Math.toDegrees(angle);
+		double variable = motor1 ? 170 : 770; // 170 = motor 1, 770 = motor 2
+
+		double finalPWM = angle * 10 + variable;
+		if (finalPWM < 1000)
+			finalPWM = 1000;
+		if (finalPWM > 2000)
+			finalPWM = 2000;
+		return String.valueOf((int) finalPWM);
+	}
+
+	public void save_xy() {
+		state = 0;
+		String fname = UIFileChooser.save();
+		if (drawing != null)
+			drawing.save_path(fname);
+	}
+
+	public void enter_path_xy() {
+		state = 2;
+	}
+
+	public void inverse() {
+		state = 1;
+		arm.draw();
+	}
+
+	public void load_xy() {
+		state = 0;
+		String fname = UIFileChooser.open();
+		drawing.load_path(fname);
+		drawing.draw();
+
+		arm.draw();
+	}
+
+	// save angles into the file
+	public void save_ang() {
+		String fname = UIFileChooser.open();
+		tool_path.convert_drawing_to_angles(drawing, arm, fname);
+	}
+
+	public void load_ang() {
+	}
+
+	public void run() {
+		while (true) {
+			arm.draw();
+			UI.sleep(20);
+		}
+	}
+
+	public static void main(String[] args) {
+		Main obj = new Main();
+	}
 
 }
--- SARC arm/src/ToolPath.java
+++ SARC arm/src/ToolPath.java
@@ -1,6 +1,4 @@
 
-
-
 /**
  * ToolPath stores motor contol signals (pwm)
  * and motor angles
@@ -11,12 +9,15 @@
  * @1000000.0
  */
 import ecs100.UI;
+import ecs100.UIFileChooser;
+
 import java.util.*;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 import java.io.Writer;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -24,100 +25,104 @@ import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+public class ToolPath {
+	int n_steps; // straight line segmentt will be broken
+					// into that many sections
 
-public class ToolPath
-{
-     int n_steps; //straight line segmentt will be broken
-                      // into that many sections
-                      
-     // storage for angles and 
-     // moto control signals
-     ArrayList<Double> theta1_vector;
-     ArrayList<Double> theta2_vector;
-     ArrayList<Integer> pen_vector;
-     ArrayList<Integer> pwm1_vector;
-     ArrayList<Integer> pwm2_vector;
-     ArrayList<Integer> pwm3_vector;
+	// storage for angles and
+	// moto control signals
+	ArrayList<Double> theta1_vector;
+	ArrayList<Double> theta2_vector;
+	ArrayList<Integer> pen_vector;
+	ArrayList<Integer> pwm1_vector;
+	ArrayList<Integer> pwm2_vector;
+	ArrayList<Integer> pwm3_vector;
 
-    /**
-     * Constructor for objects of class ToolPath
-     */
-    public ToolPath()
-    {
-        // initialise instance variables
-      n_steps = 50;
-      theta1_vector = new ArrayList<Double>();
-      theta2_vector = new ArrayList<Double>();
-      pen_vector = new ArrayList<Integer>();
-      pwm1_vector = new ArrayList<Integer>();
-      pwm2_vector = new ArrayList<Integer>();
-      pwm3_vector = new ArrayList<Integer>();
+	/**
+	 * Constructor for objects of class ToolPath
+	 */
+	public ToolPath() {
+		// initialise instance variables
+		n_steps = 50;
+		theta1_vector = new ArrayList<Double>();
+		theta2_vector = new ArrayList<Double>();
+		pen_vector = new ArrayList<Integer>();
+		pwm1_vector = new ArrayList<Integer>();
+		pwm2_vector = new ArrayList<Integer>();
+		pwm3_vector = new ArrayList<Integer>();
 
-    }
+	}
 
-    /**********CONVERT (X,Y) PATH into angles******************/
-    public void convert_drawing_to_angles(Drawing drawing,Arm arm,String fname){
+	/********** CONVERT (X,Y) PATH into angles ******************/
+	public void convert_drawing_to_angles(Drawing drawing, Arm arm, String fname) {
 
-        // for all points of the drawing...        
-        for (int i = 0;i < drawing.get_drawing_size()-1;i++){ 
-            // take two points
-            PointXY p0 = drawing.get_drawing_point(i);
-            PointXY p1 = drawing.get_drawing_point(i+1);
-            // break line between points into segments: n_steps of them
-            for ( int j = 0 ; j< n_steps;j++) { // break segment into n_steps str. lines
-                double x = p0.get_x() + j*(p1.get_x()-p0.get_x())/n_steps;
-                double y = p0.get_y() + j*(p1.get_y()-p0.get_y())/n_steps;
-                arm.inverseKinematic(x, y);
-                theta1_vector.add(arm.get_theta1()*180/Math.PI);
-                theta2_vector.add(arm.get_theta2()*180/Math.PI);
-                if (p0.get_pen()){ 
-                  pen_vector.add(1);
-                } else {
-                  pen_vector.add(0);
-                }
-            }
-        }
-    }
-    
-    public void save_angles(String fname){
-        for ( int i = 0 ; i < theta1_vector.size(); i++){
-         UI.printf(" t1=%3.1f t2=%3.1f pen=%d\n",
-            theta1_vector.get(i),theta2_vector.get(i),pen_vector.get(i));
-        }
-        
-         try {
-            //Whatever the file path is.
-            File statText = new File(fname);
-            FileOutputStream is = new FileOutputStream(statText);
-            OutputStreamWriter osw = new OutputStreamWriter(is);    
-            Writer w = new BufferedWriter(osw);
-            String str_out;
-            for (int i = 1; i < theta1_vector.size() ; i++){
-                str_out = String.format("%3.1f,%3.1f,%d\n",
-                  theta1_vector.get(i),theta2_vector.get(i),pen_vector.get(i));
-                w.write(str_out);
-            }
-            w.close();
-        } catch (IOException e) {
-            UI.println("Problem writing to the file statsTest.txt");
-        }
-        
-    }
-    
-    // takes sequence of angles and converts it 
-    // into sequence of motor signals
-    public void convert_angles_to_pwm(Arm arm){
-        // for each angle
-        for (int i=0 ; i < theta1_vector.size();i++){
-            arm.set_angles(theta1_vector.get(i),theta2_vector.get(i));
-            pwm1_vector.add(arm.get_pwm1());
-            pwm2_vector.add(arm.get_pwm2());
-        }
-    }
-    
-    // save file with motor control values
-    public void save_pwm_file(){
-        //...
-    }
+		// for all points of the drawing...
+		for (int i = 0; i < drawing.get_drawing_size() - 1; i++) {
+			// take two points
+			PointXY p0 = drawing.get_drawing_point(i);
+			PointXY p1 = drawing.get_drawing_point(i + 1);
+			// break line between points into segments: n_steps of them
+			for (int j = 0; j < n_steps; j++) { // break segment into n_steps str. lines
+				double x = p0.get_x() + j * (p1.get_x() - p0.get_x()) / n_steps;
+				double y = p0.get_y() + j * (p1.get_y() - p0.get_y()) / n_steps;
+				arm.inverseKinematic(x, y);
+				theta1_vector.add(arm.get_theta1() * 180 / Math.PI);
+				theta2_vector.add(arm.get_theta2() * 180 / Math.PI);
+				if (p0.get_pen()) {
+					pen_vector.add(1);
+				} else {
+					pen_vector.add(0);
+				}
+			}
+		}
+	}
+
+	public void save_angles(String fname) {
+		for (int i = 0; i < theta1_vector.size(); i++) {
+			UI.printf(" t1=%3.1f t2=%3.1f pen=%d\n", theta1_vector.get(i), theta2_vector.get(i), pen_vector.get(i));
+		}
+
+		try {
+			// Whatever the file path is.
+			File statText = new File(fname);
+			FileOutputStream is = new FileOutputStream(statText);
+			OutputStreamWriter osw = new OutputStreamWriter(is);
+			Writer w = new BufferedWriter(osw);
+			String str_out;
+			for (int i = 1; i < theta1_vector.size(); i++) {
+				str_out = String.format("%3.1f,%3.1f,%d\n", theta1_vector.get(i), theta2_vector.get(i),
+						pen_vector.get(i));
+				w.write(str_out);
+			}
+			w.close();
+		} catch (IOException e) {
+			UI.println("Problem writing to the file statsTest.txt");
+		}
+
+	}
+
+	// takes sequence of angles and converts it
+	// into sequence of motor signals
+	public void convert_angles_to_pwm(Arm arm) {
+		// for each angle
+		for (int i = 0; i < theta1_vector.size(); i++) {
+			arm.set_angles(theta1_vector.get(i), theta2_vector.get(i));
+			pwm1_vector.add(arm.get_pwm1());
+			pwm2_vector.add(arm.get_pwm2());
+		}
+	}
+
+	// save file with motor control values
+	public void save_pwm_file() {
+		try {
+			File f = new File(UIFileChooser.save());
+			PrintStream p = new PrintStream(f);
+			for (int i = 0; i < pwm1_vector.size(); i++) {
+				p.println(pwm1_vector.get(i)+","+pwm2_vector.get(i)+","+"2000");
+			}
+		} catch (IOException e) {
+			UI.println("Something went wrong " + e);
+		}
+	}
 
 }
